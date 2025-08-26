3ca235867d108f348a68d977d370d5b47250363a
andrewzgheib <118852647+andrewzgheib@users.noreply.github.com>
2025-08-26 23:40:38 +0300

    ui: enhanced tags page design

--- app/tags/page.tsx
+++ app/tags/page.tsx
@@ -10,32 +10,40 @@ export default async function Page() {
   const tagCounts = tagData as Record<string, number>
   const tagKeys = Object.keys(tagCounts)
   const sortedTags = tagKeys.sort((a, b) => tagCounts[b] - tagCounts[a])
+
   return (
-    <>
-      <div className="flex flex-col items-start justify-start divide-y divide-gray-200 dark:divide-gray-700 md:mt-24 md:flex-row md:items-center md:justify-center md:space-x-6 md:divide-y-0">
-        <div className="space-x-2 pb-8 pt-6 md:space-y-5">
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:border-r-2 md:px-6 md:text-6xl md:leading-14">
+    <div className="flex flex-col items-center justify-center mt-10">
+      <div className="relative z-10 text-center px-4 mb-2">
+        <div className="mb-8">
+          <h1 className="text-3xl font-extrabold md:text-6xl text-gray-900 dark:text-gray-100 tracking-tight">
             Tags
           </h1>
         </div>
-        <div className="flex max-w-lg flex-wrap">
-          {tagKeys.length === 0 && 'No tags found.'}
-          {sortedTags.map((t) => {
-            return (
-              <div key={t} className="mb-2 mr-5 mt-2">
-                <Tag text={t} />
-                <Link
-                  href={`/tags/${slug(t)}`}
-                  className="-ml-2 text-sm font-semibold uppercase text-gray-600 dark:text-gray-300"
-                  aria-label={`View posts tagged ${t}`}
-                >
-                  {` (${tagCounts[t]})`}
-                </Link>
-              </div>
-            )
-          })}
+        <div className="max-w-lg mx-auto">
+          <p className="text-gray-600 dark:text-gray-300 mb-12">
+            Browse through our collection of tags below.
+          </p>
+          <div className="flex flex-wrap justify-center gap-2">
+            {tagKeys.length === 0 && (
+              <p className="text-gray-500 dark:text-gray-400">No tags found.</p>
+            )}
+            {sortedTags.map((t) => {
+              return (
+                <div key={t} className="mb-2">
+                  <Tag text={t} />
+                  <Link
+                    href={`/tags/${slug(t)}`}
+                    className="-ml-2 text-sm font-semibold uppercase text-gray-600 dark:text-gray-300"
+                    aria-label={`View posts tagged ${t}`}
+                  >
+                    {` (${tagCounts[t]})`}
+                  </Link>
+                </div>
+              )
+            })}
+          </div>
         </div>
       </div>
-    </>
+    </div>
   )
 }
